c35e14bdf39ab3efdd6a72f7c60ecb04359b357a
Chyld Medford <chyld.medford@gmail.com>
2012-12-07 14:27:26 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoogleAnalytics::Application.config.secret_token = '7d25b2399b0168efff32e22fcbc96820439b88543bea2a37f89a8d0c71f86d778038984f7c2be053d2ead815c446b8a4b065a85346cd7525cd419a3f509771a3'
